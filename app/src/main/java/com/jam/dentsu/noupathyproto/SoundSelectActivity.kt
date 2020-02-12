2751fb0af5117e756890c6ec544727d0653173c9
278Mt <278mt.l.meitner@gmail.com>
2020-02-12 18:05:29 +0900

    for文化

--- app/src/main/java/com/jam/dentsu/noupathyproto/SoundSelectActivity.kt
+++ app/src/main/java/com/jam/dentsu/noupathyproto/SoundSelectActivity.kt
@@ -22,68 +22,46 @@ class SoundSelectActivity : AppCompatActivity() {
         val currentDataset = intent.getStringExtra("currentDataset")
         val result = Intent()
 
-        soundBtn1_1.setOnClickListener { regardingSoundBtn(1, 1) }
-        soundBtn1_2.setOnClickListener { regardingSoundBtn(1, 2) }
-        soundBtn1_3.setOnClickListener { regardingSoundBtn(1, 3) }
-        soundBtn1_4.setOnClickListener { regardingSoundBtn(1, 4) }
-        soundBtn1_5.setOnClickListener { regardingSoundBtn(1, 5) }
+        val soundBtnList = listOf(
+            listOf(soundBtn1_1, soundBtn1_2, soundBtn1_3, soundBtn1_4, soundBtn1_5),
+            listOf(soundBtn2_1, soundBtn2_2, soundBtn2_3, soundBtn2_4, soundBtn2_5),
+            listOf(soundBtn3_1, soundBtn3_2, soundBtn3_3, soundBtn3_4, soundBtn3_5)
+        )
 
-        soundBtn2_1.setOnClickListener { regardingSoundBtn(2, 1) }
-        soundBtn2_2.setOnClickListener { regardingSoundBtn(2, 2) }
-        soundBtn2_3.setOnClickListener { regardingSoundBtn(2, 3) }
-        soundBtn2_4.setOnClickListener { regardingSoundBtn(2, 4) }
-        soundBtn2_5.setOnClickListener { regardingSoundBtn(2, 5) }
-
-        soundBtn3_1.setOnClickListener { regardingSoundBtn(3, 1) }
-        soundBtn3_2.setOnClickListener { regardingSoundBtn(3, 2) }
-        soundBtn3_3.setOnClickListener { regardingSoundBtn(3, 3) }
-        soundBtn3_4.setOnClickListener { regardingSoundBtn(3, 4) }
-        soundBtn3_5.setOnClickListener { regardingSoundBtn(3, 5) }
-
-        use1.setOnClickListener {
-            setSoundSetID(currentDataset, 1)
-            setResult(Activity.RESULT_OK, result)
-            finish()
+        for (i in 0 until 3) {
+            for (j in 0 until 5) {
+                soundBtnList[i][j].setOnClickListener {
+                    val intent = Intent(this, PostSoundSelectActivity::class.java)
+                    intent.putExtra("soundNumArray", arrayOf(i, j))
+                    println(intent.getIntArrayExtra("soundNumArray"))
+                    startActivityForResult(intent, 1)
+                }
+            }
         }
 
-        use2.setOnClickListener {
-            setSoundSetID(currentDataset, 2)
-            setResult(Activity.RESULT_OK, result)
-            finish()
-        }
+        val useList = listOf(use1, use2, use3)
 
-        use3.setOnClickListener {
-            setSoundSetID(currentDataset, 3)
-            setResult(Activity.RESULT_OK, result)
-            finish()
+        for (i in 0 until 3) {
+            useList[i].setOnClickListener {
+                setSoundSetID(currentDataset, i+1)
+                setResult(Activity.RESULT_OK, result)
+                finish()
+            }
         }
 
         println("sound1_1 = ${sound1_1}")
 
-        sound1_1.setOnClickListener { sp.play(sArray[0][0], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound1_2.setOnClickListener { sp.play(sArray[0][1], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound1_3.setOnClickListener { sp.play(sArray[0][2], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound1_4.setOnClickListener { sp.play(sArray[0][3], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound1_5.setOnClickListener { sp.play(sArray[0][4], 1.0f, 1.0f, 0, 0, 1.0f) }
+        val soundList = listOf(
+            listOf(sound1_1, sound1_2, sound1_3, sound1_4, sound1_5),
+            listOf(sound2_1, sound2_2, sound2_3, sound2_4, sound2_5),
+            listOf(sound3_1, sound3_2, sound3_3, sound3_4, sound3_5)
+        )
 
-        sound2_1.setOnClickListener { sp.play(sArray[1][0], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound2_2.setOnClickListener { sp.play(sArray[1][1], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound2_3.setOnClickListener { sp.play(sArray[1][2], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound2_4.setOnClickListener { sp.play(sArray[1][3], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound2_5.setOnClickListener { sp.play(sArray[1][4], 1.0f, 1.0f, 0, 0, 1.0f) }
-
-        sound3_1.setOnClickListener { sp.play(sArray[2][0], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound3_2.setOnClickListener { sp.play(sArray[2][1], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound3_3.setOnClickListener { sp.play(sArray[2][2], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound3_4.setOnClickListener { sp.play(sArray[2][3], 1.0f, 1.0f, 0, 0, 1.0f) }
-        sound3_5.setOnClickListener { sp.play(sArray[2][4], 1.0f, 1.0f, 0, 0, 1.0f) }
-    }
-
-    private fun regardingSoundBtn(row: Int, col: Int) {
-        val intent = Intent(this, PostSoundSelectActivity::class.java)
-        intent.putExtra("soundNumArray", arrayOf(row, col))
-        println(intent.getIntArrayExtra("soundNumArray"))
-        startActivityForResult(intent, 1)
+        for (i in 0 until 3) {
+            for (j in 0 until 5) {
+                soundList[i][j].setOnClickListener { sp.play(sArray[i][j], 1.0f, 1.0f, 0, 0, 1.0f) }
+            }
+        }
     }
 
     private fun loadSounds() {
@@ -98,23 +76,17 @@ class SoundSelectActivity : AppCompatActivity() {
             .setMaxStreams(5)
             .build()
 
-        sArray[0][0] = sp.load(this, R.raw.s0_0, 1)
-        sArray[0][1] = sp.load(this, R.raw.s0_1, 1)
-        sArray[0][2] = sp.load(this, R.raw.s0_2, 1)
-        sArray[0][3] = sp.load(this, R.raw.s0_3, 1)
-        sArray[0][4] = sp.load(this, R.raw.s0_4, 1)
+        var RRawAddress = mutableListOf(
+            mutableListOf(R.raw.s0_0, R.raw.s0_1, R.raw.s0_2, R.raw.s0_3, R.raw.s0_4),
+            mutableListOf(R.raw.s1_0, R.raw.s1_1, R.raw.s1_2, R.raw.s1_3, R.raw.s1_4),
+            mutableListOf(R.raw.s2_0, R.raw.s2_1, R.raw.s2_2, R.raw.s2_3, R.raw.s2_4)
+        )
 
-        sArray[1][0] = sp.load(this, R.raw.s1_0, 1)
-        sArray[1][1] = sp.load(this, R.raw.s1_1, 1)
-        sArray[1][2] = sp.load(this, R.raw.s1_2, 1)
-        sArray[1][3] = sp.load(this, R.raw.s1_3, 1)
-        sArray[1][4] = sp.load(this, R.raw.s1_4, 1)
-
-        sArray[2][0] = sp.load(this, R.raw.s2_0, 1)
-        sArray[2][1] = sp.load(this, R.raw.s2_1, 1)
-        sArray[2][2] = sp.load(this, R.raw.s2_2, 1)
-        sArray[2][3] = sp.load(this, R.raw.s2_3, 1)
-        sArray[2][4] = sp.load(this, R.raw.s2_4, 1)
+        for (i in 0 until 3) {
+            for (j in 0 until 5) {
+                sArray[i][j] = sp.load(this, RRawAddress[i][j], 1)
+            }
+        }
 
     }
 }
